35458b3e0dd6f36e9674c9a850e7bb5cbd4b6094
Princeton Arulnesan <princeton.arul@gmail.com>
2020-08-12 19:10:07 +0000

    added some more features to the code

--- luhn.js
+++ luhn.js
@@ -1,16 +1,27 @@
 const check = function(number) {
   //from end, every second number, double it
+  let newNumber = number.slice(0, number.length - 1);
+  numLength = newNumber.length;
+  //newNumber = Number(newNumber);
   let result = 0;
   let newNum = 0;
-  for (let i = number.length; i > 0; i-2) {
-    if (number[i] * 2 > 9) {
-      result = number[i] * 2;
-      newNum += result[0] + result[1] 
+  console.log(newNumber[0]);
+  for (let i = numLength - 1; i >= 0; i -= 2) {
+    if (Number(newNumber[i]) * 2 > 9) {
+      console.log("hello")
+      result = Number(newNumber[i]) * 2;
+      result = result.toString();
+      console.log(typeof result);
+      console.log(result);
+      newNum += Number(result[0]) + Number(result[1]);
     }
     else {
-      newNum += number[i] * 2;
+      console.log("hello else")
+      newNum += Number(newNumber[i]) * 2;
     }
   }
+  console.log(newNum);
+  number[number.length - 1]
   if (newNum % 10 === 0) {
     return true;
   }
@@ -19,4 +30,4 @@ const check = function(number) {
   }
 }
 
-console.log(check(7992739871));
+console.log(check("7992739871x"));
