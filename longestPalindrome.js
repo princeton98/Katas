9a9638d2882e155afe09dff85197bf7f414ebbc3
princeton98 <princeton.arul@gmail.com>
2020-08-27 16:53:04 -0400

    refactored code

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -1,18 +1,18 @@
 const longestPalindrome = function(s) {
     // find the palindrome of the string
-    let finalPalindrome = ""
-    // then compare original string with palindrome string
+    let palindrome = "";
+    let finalPalindrome = "";
     for (let i = 0; i < s.length; i++) {
+        finalPalindrome = s[0];
         for (let j = s.length - 1; j > i ; j--) {
             if (s[i] === s[j]) {
                 let longPalindrome = "";
                 for (let k = j; k >= i; k--) {
                     longPalindrome += s[k];
-                    //console.log(longPalindrome);
                 }
                 if (s.includes(longPalindrome) && (longPalindrome.length > finalPalindrome.length)) {
                     finalPalindrome = longPalindrome;
-                    if (i === 0 && j === s.length - 1) {
+                    if (i === 0 && j === s.length -1) {
                         return finalPalindrome;
                     }
                     return finalPalindrome;
@@ -23,4 +23,4 @@ const longestPalindrome = function(s) {
     return finalPalindrome;
 }
 
-console.log(longestPalindrome("cbbd"));
+console.log(longestPalindrome(""));
