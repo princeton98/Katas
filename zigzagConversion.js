4eeb1d0b8b2e7954f4b25c2b1f548a70c9e37d74
princeton98 <princeton.arul@gmail.com>
2020-08-31 17:10:06 -0400

    done basic version of code

--- zigzagConversion.js
+++ zigzagConversion.js
@@ -5,28 +5,32 @@ const convert = function(s, numRows) {
     // numRows determines the variables
     let rows = [];
     let repeatSpot = numRows;
+    let finalWord = "";
     for (let l = numRows - 2; l > 0; l--) {
         repeatSpot++;
     }
     console.log(repeatSpot);
     for (let i = 0; i < numRows; i++) {
         rows.push([s[i]]);;
-        console.log(rows);
     }
     for (let k = 0; k < s.length; k += repeatSpot) {
         let j = 0;
         if (k !== 0) {
             for (let i = 0; i < numRows; i ++) {
-                rows[i].push(s[k + i]);
+                if (s[k + i]) {
+                    rows[i].push(s[k + i]);
+                }
             }
         }
-        while ((numRows - 2) - j !== 0) {
-            rows[(numRows - 2) - j].push(s[numRows + k - j]);
+        while ((numRows - 2) - j !== 0 && s[numRows + k + j]) {
+            rows[(numRows - 2) - j].push(s[numRows + k + j]);
             j++;
         }
     }
-    
-    return rows;
+    for (let i = 0; i < numRows; i++) {
+        finalWord += rows[i].join("");
+    }
+    return finalWord;
 }
 
 console.log(convert("PAYPALISHIRING", 3));
