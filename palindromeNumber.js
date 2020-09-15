af6e2d029df8871001ba5ce44a8eda8c48bd1f67
princeton98 <princeton.arul@gmail.com>
2020-09-15 18:49:26 -0400

    finished up code

--- palindromeNumber.js
+++ palindromeNumber.js
@@ -1,4 +1,16 @@
 const isPalindrome = function(x) {
     // x is an integer
     // change into string, start from back, input numbers backwards, compare to original
+    const num = x.toString();
+    let palindrome = ""
+    console.log(num);
+    for (let i = num.length - 1; i >= 0; i--) {
+        palindrome += num[i]
+    }
+    if (palindrome === num) {
+        return true
+    } else {
+        return false;
+    }
 }
+console.log(isPalindrome(121));
