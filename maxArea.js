da6368f049acdea7dc407d69656c0aec92ae7e21
princeton98 <princeton.arul@gmail.com>
2020-09-16 15:35:52 -0400

    finished up code

--- maxArea.js
+++ maxArea.js
@@ -1,5 +1,25 @@
 const maxArea = function(height) {
     // height is an array
     // first, find the two lines that formulate the container
-    
+    let area = 0;
+    let finalArea = 0;
+    for (let i = 0; i < height.length; i++) {
+        for (let j = height.length - 1; j > i; j--) {
+            if (height[i] < height[j]) {
+                area = height[i] * (j - i);
+                if (area > finalArea) {
+                    finalArea = area;
+                }
+            }
+            else {
+                area = height[j] * (j - i);
+                if (area > finalArea) {
+                    finalArea = area;
+                }
+            }
+        }
+    }
+    return finalArea;
 }
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]))
