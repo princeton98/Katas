1c8572442e6feee3db5acdad0da0e3c73f29a06f
princeton98 <princeton.arul@gmail.com>
2021-10-28 22:08:35 -0400

    first part of leetcode problem

--- validParentheses.js
+++ validParentheses.js
@@ -1,3 +1,20 @@
 const isValid = function(s) {
-    //
+    let arr = s.split("");
+    let valid = false;
+    for (let i = 0; i < arr.length; i++) {
+        if ((arr[i] === "(" && arr[i+1] === ")") || (arr[i] === "(" && arr[arr.length - 1 - i] === ")")) {
+            valid = true;
+        }
+          else if ((arr[i] === "{" && arr[i+1] === "}") || (arr[i] === "{" && arr[arr.length - 1 - i] === "}")) {
+            valid = true;
+        } else if  ((arr[i] === "[" && arr[i+1] === "]") || (arr[i] === "[" && arr[arr.length - 1 - i] === "]")) {
+            valid = true;
+        } else if (arr[i] === ")" || arr[i] === "}" || arr[i] === "]") {
+        } else {
+            return valid = false;
+        }
+    }
+    return valid;
 }
+
+console.log(isValid("(}{)"));
